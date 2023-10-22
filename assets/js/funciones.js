5168d6547f8b03b47d0103463c88e64af19805ac
arath abello <ar.abello@duocuc.cl>
2023-10-22 20:36:46 -0300

    variable que toma el valor del input y se le asigna al contenido

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,14 +1,17 @@
 let idCounter = 0;
+let input = document.getElementById('InputTextMain').value;
+
 let list = document.getElementById('list');
 export let addTask = () => {
     idCounter++;
+    let newValue = input;
 
     // hacemos referencia a la variable y a esta misma variable le añadimos un parrafo con este valor (en vez de reemplazar el elemento anterior pues que se añada más parrafos con este valor)
     list = list.innerHTML += `
     <div class="task-container" id="${idCounter}">
         <label>
             <input type="checkbox">
-            Tarea
+            ${newValue}
         </label>
         <img src="assets/images/trash-can-solid.svg" alt="icono de basurero para eliminar la tarea agregada" class="closeBtn">
     </div>`;
